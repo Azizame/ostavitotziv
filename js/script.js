450968a5a6dbe69f5fbe7d34e6e57264727619a6
Azizame <mahamatova0@gmail.com>
2023-10-30 21:48:00 +0500

    last

--- js/script.js
+++ js/script.js
@@ -1,50 +1,3 @@
-const splide = new Splide('.slider_box .splide', { 
-    type: 'slide',
-    
-} );
-splide.on( 'overflow', function ( isOverflow ) {
-  // Reset the carousel position
-  splide.go( 0 );
-
-  splide.options = {
-    arrows    : isOverflow,
-    pagination: isOverflow,
-    drag      : isOverflow,
-    clones    : isOverflow ? undefined : 0, // Toggle clones
-  };
-} );
-splide.mount();
-
-var splideGallery = new Splide( '.gallery_box #main-carousel', {
-  pagination: false,
-} );
-
-var thumbnails = document.getElementsByClassName( 'thumbnail' );
-var current;
-
-for ( var i = 0; i < thumbnails.length; i++ ) {
-  initThumbnail( thumbnails[ i ], i );
-}
-
-function initThumbnail( thumbnail, index ) {
-  thumbnail.addEventListener( 'click', function () {
-    splide.go( index );
-  } );
-}
-
-splideGallery.on( 'mounted move', function () {
-  var thumbnail = thumbnails[ splideGallery.index ];
-
-  if ( thumbnail ) {
-    if ( current ) {
-      current.classList.remove( 'is-active' );
-    }
-
-    thumbnail.classList.add( 'is-active' );
-    current = thumbnail;
-  }
-} );
-splideGallery.mount();
 function myAbout() {
   var x = document.getElementById("myAbout");
   if (x.className === "fa fa-heart") {
@@ -53,3 +6,10 @@ function myAbout() {
     x.className = "fa fa-heart";
   }
 }
+window.addEventListener("scroll", function(){
+  let header = document.getElementById("header");
+  let section = document.getElementById("section")
+  console.log(header, "aziza");
+  header.classList.toggle("sticky", window.scrollY > 55)
+  section.classList.toggle("first-m", window.scrollY > 55)
+})
